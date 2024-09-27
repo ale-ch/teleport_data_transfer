5bf649abbab525d103631b822922b5990cd40509
Chiodin Alessio <ieo7086@cn07.cluster.loc>
2024-09-27 16:08:40 +0200

    Delete run script

--- run_data_transfer.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-/hpcnfs/techunits/imaging/work/ATTEND/achiodin/data_transfer_utils/2_transfer_data.sh --src-list /hpcnfs/techunits/imaging/work/ATTEND/achiodin/directories.txt --dst /home/ieo7086/work/ome_tiff_images
